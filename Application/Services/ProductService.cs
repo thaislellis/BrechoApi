73e581dd16c369caf8cf9c1fecedeff6eef35821
Thais Lellis <thais123lm@gmail.com>
2026-04-12 21:21:33 -0300

    feat: add product service

---
+++ Application/Services/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using BrechoApi.Application.DTOs.Product;
+using BrechoApi.Application.Interfaces;
+using BrechoApi.Domain.Entities;
+
+namespace BrechoApi.Application.Services
+{
+    public class ProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<Product> CreateAsync(CreateProductDto dto)
+        {
+            var product = new Product
+            {
+                Name = dto.Name,
+                Description = dto.Description,
+                Category = dto.Category,
+                Price = dto.Price,
+            };
+            return await _productRepository.CreateAsync(product);
+        }
+        public async Task<List<Product>> GetAllAsync()
+        {
+            return await _productRepository.GetAllAsync();
+        }
+        public async Task<Product?> GetByIdAsync(Guid id)
+        {
+            return await _productRepository.GetByIdAsync(id);
+        }
+    }
+}
