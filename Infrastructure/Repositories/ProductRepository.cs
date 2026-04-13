3c8d14301d16a1e861f664247ab809c1aeae64ee
Thais Lellis <thais123lm@gmail.com>
2026-04-12 21:21:14 -0300

    feat: add product repository implementation

---
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using BrechoApi.Application.Interfaces;
+using BrechoApi.Domain.Entities;
+using BrechoApi.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrechoApi.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+        public async Task<Product?> GetByIdAsync(Guid id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<List<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
