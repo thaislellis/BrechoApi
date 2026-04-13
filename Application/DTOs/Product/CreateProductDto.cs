53f6ddefc0c939ee744d64db253552098eee36e0
Thais Lellis <thais123lm@gmail.com>
2026-04-12 21:20:44 -0300

    feat: add create product DTO

---
+++ Application/DTOs/Product/CreateProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BrechoApi.Application.DTOs.Product
+{
+    public class CreateProductDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Category { get; set; } = string.Empty;
+
+    }
+}
