c584094d410a014e8e0b8affad1b59c2574c18f3
Thais Lellis <thais123lm@gmail.com>
2026-04-20 19:05:15 -0300

    feat: add product controller with create and read endpoints

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using BrechoApi.Application.DTOs.Product;
+using BrechoApi.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrechoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductDto dto)
+        {
+            var product = await _productService.CreateAsync(dto);
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+     
+        }
+    }
+}
